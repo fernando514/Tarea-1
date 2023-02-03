a30af8e1fffb52851325a01053658ce6b22e4c28
fernando514 <117423732+fernando514@users.noreply.github.com>
2023-02-03 00:31:45 -0600

    Tarea
    Terminado ejercicio tarea

---
+++ Tarea1/Ejerciciotarea/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace Ejerciciotarea
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.numeroTextBox = new System.Windows.Forms.TextBox();
+            this.CalcularButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(103, 88);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(258, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ingrese un número:";
+            // 
+            // numeroTextBox
+            // 
+            this.numeroTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numeroTextBox.Location = new System.Drawing.Point(367, 88);
+            this.numeroTextBox.Name = "numeroTextBox";
+            this.numeroTextBox.Size = new System.Drawing.Size(173, 39);
+            this.numeroTextBox.TabIndex = 1;
+            // 
+            // CalcularButton
+            // 
+            this.CalcularButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CalcularButton.Location = new System.Drawing.Point(376, 172);
+            this.CalcularButton.Name = "CalcularButton";
+            this.CalcularButton.Size = new System.Drawing.Size(154, 59);
+            this.CalcularButton.TabIndex = 2;
+            this.CalcularButton.Text = "Calcular";
+            this.CalcularButton.UseVisualStyleBackColor = true;
+            this.CalcularButton.Click += new System.EventHandler(this.CalcularButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.CalcularButton);
+            this.Controls.Add(this.numeroTextBox);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox numeroTextBox;
+        private System.Windows.Forms.Button CalcularButton;
+    }
+}
+
---
+++ Tarea1/Ejerciciotarea/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejerciciotarea
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private int parimpar(int numero)
+        {
+            if (numero % 2 == 0)
+            {
+                MessageBox.Show("El numero es par", "Resultado", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            }
+            if (numero % 2 > 0)
+            {
+                MessageBox.Show("El numero es impar", "Resultado", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            }
+
+            return numero;
+        }
+
+        private decimal posneg(decimal numero)
+        {
+            if (numero > 0)
+            {
+                MessageBox.Show("El numero es positivo", "Resultado 2", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else if (numero < 0) 
+            {
+                MessageBox.Show("El numero es negativo", "Resultado 2", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+
+            return numero;
+        }
+        
+
+        private void CalcularButton_Click(object sender, EventArgs e)
+        {
+
+            int total = parimpar(Convert.ToInt32(numeroTextBox.Text));
+            decimal total2 = posneg(Convert.ToDecimal(numeroTextBox.Text));
+        }
+    }
+}
